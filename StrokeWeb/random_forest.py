0afb8601e075b746fb61a52e29779524a90469d4
Shounoop <hmtuan9a@gmail.com>
2022-12-15 00:34:51 +0700

    Update random_forest

--- StrokeWeb/random_forest.py
+++ StrokeWeb/random_forest.py
@@ -4,18 +4,9 @@ import pandas as pd
 import numpy as np
 
 
-# def getQttPerLabel(y):
-#     trueQtt = 0
-#     for i in y:
-#         if y[i] == 1:
-#             trueQtt += 1
-#     return np.array([len(y) - trueQtt, trueQtt])
-
-
 def tinhE(y):
     # y = [0, 1, 0] --> soLuongCacNhan = [2, 1]
     soLuongCacNhan = np.bincount(y)
-    # soLuongCacNhan = getQttPerLabel(y)
     cacXacSuat = soLuongCacNhan / len(y)
     return -np.sum([xacSuat * np.log2(xacSuat) for xacSuat in cacXacSuat if xacSuat > 0])
 
@@ -50,8 +41,8 @@ class DecisionTree:
             self.soLuongFeatures, X.shape[1])
         self.nodeGoc = self.phatTrienCay(X, y)
 
-    def predict(self, X):
-        return np.array([self._traverse_tree(x, self.nodeGoc) for x in X])
+    def duDoan(self, X):
+        return np.array([self.duyetCay(x, self.nodeGoc) for x in X])
 
     def phatTrienCay(self, X, y, doSauHienTai=0):
         soLuongMau, soLuongFeatures = X.shape
@@ -62,27 +53,29 @@ class DecisionTree:
             or soLuongNhan == 1
             or soLuongMau < self.min_samples_split
         ):
-            leaf_value = self.getLabel(y)
-            return Node(value=leaf_value)
+            leafValue = self.getValueHere(y)
+            return Node(value=leafValue)
 
         featureIndexs = np.random.choice(
             soLuongFeatures, self.soLuongFeatures, replace=False)
 
-        best_feat_index, best_nguong = self.getBestFeatIdxAndSplitThresh(
+        best_split_feat_index, best_nguong = self.getBestFeatIdxAndSplitThresh(
             X, y, featureIndexs)
 
-        conBenTraiIdxs, conBenPhaiIdxs = self._split(
-            X[:, best_feat_index], best_nguong)
-        conBenTrai = self.phatTrienCay(
-            X[conBenTraiIdxs, :], y[conBenTraiIdxs], doSauHienTai + 1)
-        conBenPhai = self.phatTrienCay(
-            X[conBenPhaiIdxs, :], y[conBenPhaiIdxs], doSauHienTai + 1)
+        nhoHonNguongIdxs, lonHonNguongIdxs = self.phanTach(
+            X[:, best_split_feat_index], best_nguong)
 
-        return Node(best_feat_index, best_nguong, conBenTrai, conBenPhai)
+        # X[nhoHonNguongIdxs, :] --> các mẫu có giá trị tại split_feature <= best_nguong
+        conBenTrai = self.phatTrienCay(
+            X[nhoHonNguongIdxs, :], y[nhoHonNguongIdxs], doSauHienTai + 1)
+        conBenPhai = self.phatTrienCay(
+            X[lonHonNguongIdxs, :], y[lonHonNguongIdxs], doSauHienTai + 1)
+
+        return Node(best_split_feat_index, best_nguong, conBenTrai, conBenPhai)
 
     def getBestFeatIdxAndSplitThresh(self, X, y, featureIndexs):
         doLoiThongTinCaoNhat = -1
-        split_idx, split_thresh = None, None
+        phanTachIdx, nguongTachTotNhat = None, None
 
         for feat_idx in featureIndexs:
             cotHienTai = X[:, feat_idx]
@@ -93,66 +86,51 @@ class DecisionTree:
 
                 if doLoiThongTin > doLoiThongTinCaoNhat:
                     doLoiThongTinCaoNhat = doLoiThongTin
-                    split_idx = feat_idx
-                    split_thresh = nguong
+                    phanTachIdx = feat_idx
+                    nguongTachTotNhat = nguong
 
-        return split_idx, split_thresh
+        return phanTachIdx, nguongTachTotNhat
 
-    def getInformationGain(self, y, cotHienTai, split_thresh):
+    def getInformationGain(self, y, cotHienTai, nguongTach):
         parentE = tinhE(y)
-        conBenTraiIdxs, conBenPhaiIdxs = self._split(
-            cotHienTai, split_thresh)
-
-        if len(conBenTraiIdxs) == 0 or len(conBenPhaiIdxs) == 0:
-            return 0
+        nhoHonNguongIdxs, lonHonNguongIdxs = self.phanTach(
+            cotHienTai, nguongTach)
 
         n = len(y)
-        numberLeft, numberRight = len(conBenTraiIdxs), len(conBenPhaiIdxs)
-        eLeft, eRight = tinhE(y[conBenTraiIdxs]), tinhE(y[conBenPhaiIdxs])
-        weightedAvg_ChildE = (numberLeft / n) * eLeft + \
-            (numberRight / n) * eRight
+        soLuongNhoHonNguong, soLuongLonHonNguong = len(
+            nhoHonNguongIdxs), len(lonHonNguongIdxs)
+        eLeft, eRight = tinhE(y[nhoHonNguongIdxs]), tinhE(
+            y[lonHonNguongIdxs])
+        weightedAvg_childE = (soLuongNhoHonNguong / n) * eLeft + \
+            (soLuongLonHonNguong / n) * eRight
 
-        informationGain = parentE - weightedAvg_ChildE
+        informationGain = parentE - weightedAvg_childE
         return informationGain
 
-    def _split(self, X_column, split_thresh):
-        # print(split_thresh)
-        conBenTraiIdxs = np.argwhere(X_column <= split_thresh).flatten()
-        conBenPhaiIdxs = np.argwhere(X_column >= split_thresh).flatten()
-        return conBenTraiIdxs, conBenPhaiIdxs
+    def phanTach(self, X_column, nguongTach):
+        nhoHonNguongIdxs = np.argwhere(X_column <= nguongTach).flatten()
+        lonHonNguongIdxs = np.argwhere(X_column >= nguongTach).flatten()
+        return nhoHonNguongIdxs, lonHonNguongIdxs
 
-    def _traverse_tree(self, x, node):
+    def duyetCay(self, x, node):
         if node.laNodeLa():
             return node.value
 
         if x[node.feature] <= node.nguong:
-            return self._traverse_tree(x, node.conBenTrai)
-        return self._traverse_tree(x, node.conBenPhai)
+            return self.duyetCay(x, node.conBenTrai)
+        return self.duyetCay(x, node.conBenPhai)
 
     # getMostCommonLabel
-    def getLabel(self, y):
+    def getValueHere(self, y):
         counter = Counter(y)
-
-        # if (len(counter) != 0):
-        #     most_common = counter.most_common(1)[0][0]
-        #     return most_common
-        # else:
-        #     return [0]
-
         label = counter.most_common(1)[0][0]
         return label
 
 
-def bootstrap_sample(X, y):
+def randomSamples(X, y):
     soLuongMau = X.shape[0]
-    idxs = np.random.choice(soLuongMau, soLuongMau, replace=True)
-    return X[idxs], y[idxs]
-
-
-def most_commonumberLeftabel(y):
-    counter = Counter(y)
-    most_common = counter.most_common(1)[0][0]
-    return most_common
+    randomIdxs = np.random.choice(soLuongMau, soLuongMau, replace=True)
+    return X[randomIdxs], y[randomIdxs]
 
 
 class RandomForest:
@@ -171,16 +149,22 @@ class RandomForest:
                 doSauToiDa=self.doSauToiDa,
                 soLuongFeatures=self.soLuongFeatures,
             )
-            X_samp, y_samp = bootstrap_sample(X, y)
+            X_samp, y_samp = randomSamples(X, y)
             tree.fit(X_samp, y_samp)
             self.trees.append(tree)
 
-    def predict(self, X):
-        tree_preds = np.array([tree.predict(X) for tree in self.trees])
+    def duDoan(self, X):
+        tree_preds = np.array([tree.duDoan(X) for tree in self.trees])
         tree_preds = np.swapaxes(tree_preds, 0, 1)
-        y_pred = [most_commonumberLeftabel(tree_pred)
+
+        y_pred = [self.getValueHere(tree_pred)
                   for tree_pred in tree_preds]
         return np.array(y_pred)
+
+    def getValueHere(self, y):
+        counter = Counter(y)
+        label = counter.most_common(1)[0][0]
+        return label
 
 
 if __name__ == "__main__":
@@ -190,7 +174,7 @@ if __name__ == "__main__":
 
     data = pd.read_csv("./stroke_final.csv")
 
-    features = ["gender", "age", "hypertension"]
+    features = ["gender", "age", "hypertension", "smoking_status"]
 
     X = data[features].to_numpy()[0: 1000]
     y = data["stroke"].to_numpy()[0: 1000]
@@ -202,7 +186,7 @@ if __name__ == "__main__":
     clf = RandomForest(n_trees=3, doSauToiDa=10)
 
     clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
+    y_pred = clf.duDoan(X_test)
     acc = accuracy(y_test, y_pred)
 
     print("Accuracy:", acc)
