f5c8b6d8c9c4e1d123896022dcbd9fe1d6b39d4b
Minh <phungtanminh01@gmail.com>
2022-12-17 10:57:02 +0700

    add comment bayes

--- StrokeWeb/bayes.py
+++ StrokeWeb/bayes.py
@@ -15,7 +15,7 @@ class NaiveBayes:
         # Giá trị trung bình của môĩ fearture ưng voi môi nhãn
         self.trungbinh = np.zeros((sl_nhan, sl_dactrung), dtype=np.float64)
         self.phuongsai = np.zeros((sl_nhan, sl_dactrung), dtype=np.float64)
-        self.tansuat_nhan = np.zeros(sl_nhan, dtype=np.float64)
+        self.xacsuat_tiennghiem = np.zeros(sl_nhan, dtype=np.float64)
 
         for idx, giatri_nhan in enumerate(self.nhan):
             # Lâý các mâũ có nhãn băng vơi giá trị của tưng nhãn
@@ -24,7 +24,7 @@ class NaiveBayes:
             self.phuongsai[idx, :] = X_nhan.var(axis=0)
             # Xác định tân suât của nhãn đó trong tâp dư liêu
             # X_c.shape[0] sô lương mâũ của nhãn
-            self.tansuat_nhan[idx] = X_nhan.shape[0] / float(sl_mau)
+            self.xacsuat_tiennghiem[idx] = X_nhan.shape[0] / float(sl_mau)
 
     def predict(self, X):
         dudoan = [self._predict(x) for x in X]
@@ -34,13 +34,12 @@ class NaiveBayes:
         mang_xacsuat_nhan = []
         # Tính xăc suât hâụ nghiêm cho các nhãn
         for idx, giatri_nhan in enumerate(self.nhan):
-            xacsuat_nhan = np.sum(np.log(self._pdf(idx, x))) + \
-                np.log(self.tansuat_nhan[idx])
-            mang_xacsuat_nhan.append(xacsuat_nhan)
+            xacsuat_haunghiem = np.sum(np.log(self.xacsuat_dieukien(idx, x))) + np.log(self.xacsuat_tiennghiem[idx])
+            mang_xacsuat_nhan.append(xacsuat_haunghiem)
         # Trả vê nhãn vơi giá trị xs hâụ nghiêm max
         return self.nhan[np.argmax(mang_xacsuat_nhan)]
 
-    def _pdf(self, class_idx, x):
+    def xacsuat_dieukien(self, class_idx, x):
         # print(x)
         gt_trungbinh_nhan = self.trungbinh[class_idx]
         gt_phuongsai_nhan = self.phuongsai[class_idx]
